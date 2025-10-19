5769ea3825c0950b9b80d207b09c8dfe7dd3be8b
zi xing ng <ngxing116@gmail.com>
2025-10-19 22:54:57 +0800

    solve filter bugs

--- mypages/budget.py
+++ mypages/budget.py
@@ -14,8 +14,9 @@ class BudgetPage( PageTemplate ):
         debit = read_from_excel("Debit")
         budget = read_from_excel("Budget")
         credit = read_from_excel("Credit")
+        regular_payment = read_from_excel("Regular Payment")
 
-        if debit is not None and budget is not None and credit is not None:
+        if debit is not None and budget is not None and credit is not None and regular_payment is not None:
 
             credit["Date"] = pd.to_datetime(credit["Date"], format="%m/%d/%Y")
 
@@ -26,55 +27,82 @@ class BudgetPage( PageTemplate ):
             budget["Date"] = pd.to_datetime(budget["Date"], format="%m/%d/%Y")
             budget = budget[ ["Date", "Category", "Subcategory", "Amount"] ]
 
-            debit["Year"] = debit["Date"].dt.year
-            debit["Month"] = debit["Date"].dt.month
-            debit = debit.groupby(["Year", "Month", "Category", "Subcategory"]).agg( {"Cost": "sum"} )
 
+            regular_payment["Date"] = pd.to_datetime(regular_payment["Date"], format="%m/%d/%Y")
+            regular_payment = regular_payment[ ["Date", "Category", "Subcategory", "Amount"] ]
 
-            budget["Year"] = budget["Date"].dt.year
-            budget["Month"] = budget["Date"].dt.month
-            budget = budget.groupby(["Year", "Month", "Category", "Subcategory"], as_index = False).agg( {"Amount": "sum"} )
+            budget = pd.concat([budget, regular_payment]).reset_index()
 
-            df = debit.merge(
-                budget, 
-                left_on = ["Year", "Month", "Category", "Subcategory"],
-                right_on = ["Year", "Month", "Category", "Subcategory"],
-                how="left"
-                )
-            
-            df = df.rename(columns = {"Amount" : "Budget"})
-            df["Budget"] = df["Budget"].fillna(0)
-            df["Budget Remain"] = ( df["Budget"] - df["Cost"] ).round(2)
-
-            df["Day"] = 1
-            df["Date"] = pd.to_datetime( df[["Year", "Month", "Day"]] )
-            df.drop(columns = ["Year", "Month", "Day"])
-            
-
-            self.df = { "budget" : df, "credit": credit }
+            self.df = { "budget" : budget, "credit": credit, "debit" : debit }
 
     def render(self):
 
         if self.df is not None:
 
-            filter_condition = " ( Date >= @self.begin ) and ( Date <= @self.end )"
-            budget, credit = self.df["budget"].query(filter_condition), self.df["credit"].query(filter_condition)
-             
 
+            filter_condition = " ( Date >= @self.begin ) and ( Date <= @self.end )"
+            budget, credit, debit = self.df["budget"].query(filter_condition), self.df["credit"].query(filter_condition), self.df["debit"].query(filter_condition)
+
+            budget["Year"] = budget["Date"].dt.year
+            budget["Month"] = budget["Date"].dt.month
+            budget = budget.drop(columns = ["Date"])
+            budget = budget.groupby(["Year", "Month", "Category", "Subcategory"]).agg( {"Amount": "sum"} )
+
+            debit["Year"] = debit["Date"].dt.year
+            debit["Month"] = debit["Date"].dt.month
+            debit = debit.drop(columns = ["Date"])
+            debit = debit.groupby(["Year", "Month", "Category", "Subcategory"]).agg( {"Cost": "sum"} )
+
+            df = debit.merge(
+                budget,
+                left_on = ["Year", "Month", "Category", "Subcategory"],
+                right_on = ["Year", "Month", "Category", "Subcategory"],
+                how="outer",
+                ).reset_index()
+            
+            print(df)
+      
+            df = df.rename(columns = {"Amount" : "Budget"})
+
+        
+            df["Budget"] = df["Budget"].fillna(0)
+            df["Cost"] = df["Cost"].fillna(0)
+            df["Budget Remain"] = ( df["Budget"] - df["Cost"] ).round(2)
+
+
+            budget = df
+     
             col1, col2, col3 = st.columns([1,1, 1])
 
             total_budget = round(budget["Budget"].sum(), 2)
             total_credit = round(credit["Amount"].sum(), 2)
             budget_remain = round(budget["Budget Remain"].sum(), 2)
+            pos_budget_remain = round(budget.query("`Budget Remain` >= 0")["Budget Remain"].sum(), 2)
+            neg_budget_remain = round(budget.query("`Budget Remain` < 0")["Budget Remain"].sum(), 2)
+
+
 
             with col1:
                 st.metric("Total Budget", total_budget)
             
             with col2:
-                st.metric("Forecasted Balance", round(total_credit - total_budget,2))
+                st.metric("Planned Balance", round(total_credit - total_budget,2))
 
             with col3:
-                st.metric("Budget Remaining", budget_remain)
+                st.metric("Forecasted Balance", round(total_credit - total_budget + neg_budget_remain,2) )
+
+            
+            col4, col5, col6 = st.columns([1,1, 1])
+
+            with col4:
+                st.metric("Actual Budget Remaining", budget_remain)
+
+            with col5:
+                st.metric("Planned Budget Remaining", pos_budget_remain)
+            
+            with col6:
+                st.metric("Out Planned Expenses", neg_budget_remain)
+
 
             st.write("### 📈 Expenses Ratio of Category")
             pie_data = budget.groupby(["Category"], as_index=False).agg({"Budget":"sum"})
--- utils/page_template.py
+++ utils/page_template.py
@@ -6,10 +6,10 @@ import pandas as pd
 class PageTemplate(ABC):
 
     def __init__(self) -> None:
-        
-        self.begin = None
-        self.end = None
+
         self.df = None
+        self.set_begin()
+        self.set_end()
 
     @abstractmethod
     def processing(self) -> pd.DataFrame | None:
@@ -19,27 +19,57 @@ class PageTemplate(ABC):
     def render(self) -> None:
         pass
 
+    def set_begin(self, date = None):
+
+        if(date == None and st.session_state.get( "begin") == None):
+            self.begin = pd.Timestamp.today().replace(day=1).date()
+            st.session_state["begin"] = self.begin
+            return
+        
+        if(date != None):
+            self.begin = date
+            st.session_state["begin"] = date
+            return
+        
+        self.begin = st.session_state["begin"]
+        
+
+    def set_end(self, date = None):
+
+        if(date == None and st.session_state.get("end") == None):
+            self.end = ( pd.Timestamp.today() + pd.offsets.MonthEnd(0) ).date()
+            st.session_state["end"] = self.end
+            return
+        
+        if(date != None):
+            self.end = date
+            st.session_state["end"] = date
+            return
+        
+        self.end = st.session_state["end"]
+
+
 
     def filter_header(self) -> None:
 
         col1, col2, col3, col4 = st.columns([19, 19, 6, 6], vertical_alignment="bottom", width="stretch") 
 
-
         with col3:
             if st.button("Today", width="stretch", type="primary"):
-                self.begin = pd.Timestamp.today()
-                self.end = pd.Timestamp.today()
+                self.set_begin(pd.Timestamp.today().date())
+                self.set_end(pd.Timestamp.today().date())
 
         with col4:
             if st.button("Reset", width="stretch"):
-                self.begin = pd.Timestamp.today().replace(day=1)
-                self.end = pd.Timestamp.today() + pd.offsets.MonthEnd(0)
+                self.set_begin(pd.Timestamp.today().replace(day=1).date())
+                self.set_end( (pd.Timestamp.today() + pd.offsets.MonthEnd(0)).date() )
 
         with col1:
-            self.begin = st.date_input("Start Date",  self.begin or pd.Timestamp.today().replace(day=1) )
+            self.begin = st.date_input("Start Date", key = "begin" )
 
         with col2:
-            self.end = st.date_input("End Date", self.end or pd.Timestamp.today() + pd.offsets.MonthEnd(0) )
+            self.end = st.date_input("End Date", key = "end" )
+
 
 
 
